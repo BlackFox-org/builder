0c92774f0d7fa2bfdb288a55085c4b5448c156c8
BlackFox-org <blackfoxdsorg@gmail.com>
2025-08-27 17:52:12 +0000

    Changed firmware repo. DO NOT PR

--- builder.sh
+++ builder.sh
@@ -111,7 +111,7 @@ git pull
 rm -rf openipc
 if [ ! -d "$FIRMWARE_DIR" ]; then
     echo_c 33 "\nDownloading Firmware"
-    git clone --depth=1 https://github.com/OpenIPC/firmware.git "$FIRMWARE_DIR"
+    git clone --depth=1 https://github.com/BlackFox-org/firmware.git "$FIRMWARE_DIR"
     cd "$FIRMWARE_DIR"
 else
     echo_c 33 "\nUpdating Firmware"
